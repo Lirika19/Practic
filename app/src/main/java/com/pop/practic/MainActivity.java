dee1a136b78b502c324e4db3fe89bb3cb9342d4d
Sasha <alexandra-1902@yandex.ru>
2022-05-05 23:36:03 +0300

    на площади

--- app/src/main/java/com/pop/practic/MainActivity.java
+++ app/src/main/java/com/pop/practic/MainActivity.java
@@ -1,14 +1,29 @@
 package com.pop.practic;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.pop.practic.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        NavHostFragment navHostFragment = activityMainBinding.tabsContainer.getFragment();
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(activityMainBinding.bottomNavigationView, navController);
     }
 }
