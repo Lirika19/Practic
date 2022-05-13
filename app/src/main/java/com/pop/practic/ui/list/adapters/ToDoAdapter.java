c0b92fe2cc47c41486d5481e4e7d7176cdbe6e68
Fanlug <ytipytu1998@gmail.com>
2022-05-13 22:28:02 +0300

    кровь стекала по щекам

---
+++ app/src/main/java/com/pop/practic/ui/list/adapters/ToDoAdapter.java
@@ -0,0 +1,32 @@
+package com.pop.practic.ui.list.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View view) {
+            super(view);
+        }
+    }
+}
--- app/src/main/java/com/pop/practic/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/pop/practic/ui/settings/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.pop.practic;
+package com.pop.practic.ui.settings;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.pop.practic.R;
 
 /**
  * A simple {@link Fragment} subclass.
