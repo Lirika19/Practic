e92b4631d31a8a7cad1f59cdded5889335d097c8
Sasha <alexandra-1902@yandex.ru>
2022-05-23 22:35:29 +0300

    надеюсь я сейчас все не испорчу2

--- app/src/main/java/com/pop/practic/ui/zametki/adapters/GoalsAdapter.java
+++ app/src/main/java/com/pop/practic/ui/zametki/adapters/GoalsAdapter.java
@@ -1,5 +1,6 @@
 package com.pop.practic.ui.zametki.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -7,9 +8,22 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.pop.practic.R;
+import com.pop.practic.repository.Repository;
 import com.pop.practic.repository.room.goals.Goals;
 
+
+import java.util.List;
+
 public class GoalsAdapter extends RecyclerView.Adapter<GoalsAdapter.ViewHolder> {
+    private List<Goals> notes;
+    static Context context;
+    Repository repository;
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+        private final Button button;
+        public ViewHolder(View view) {
+            super(view);
+            button  = view.findViewById(R.id.item_row_goalds_button);
+        }
         }
 }
